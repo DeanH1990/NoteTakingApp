afa8bd63586f673d4cfdcdcf94e136191f3e5907
DeanH1990 <dhasley1990@gmail.com>
2023-06-07 17:05:38 +0100

    refactor domain note repository

--- domain/src/main/kotlin/com/example/notetakingapp/repositories/NoteRepository.kt
+++ domain/src/main/kotlin/com/example/notetakingapp/repositories/NoteRepository.kt
@@ -3,9 +3,9 @@ package com.example.notetakingapp.repositories
 import com.example.notetakingapp.models.Note
 
 interface NoteRepository {
-    fun getAllNotes(): List<Note>
-    fun getNoteById(id: Int): Note
-    fun createNote(note: Note)
-    fun updateNote(note: Note)
-    fun deleteNoteById(id: Int)
+    suspend fun getAllNotes(): List<Note>
+    suspend fun getNoteById(id: Int): Note?
+    suspend fun createNote(note: Note)
+    suspend fun updateNote(note: Note)
+    suspend fun deleteNoteById(id: Int)
 }
