a170806abb6a90c70600f164ae6777375016296b
DeanH1990 <dhasley1990@gmail.com>
2023-06-14 11:46:57 +0100

    initial home screen set up with view model

--- ui/src/main/java/com/example/ui/MainActivity.kt
+++ ui/src/main/java/com/example/ui/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.ui.ui.home.HomeScreen
 import com.example.ui.ui.theme.NoteTakingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteTakingAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NoteTakingAppTheme {
-        Greeting("Android")
     }
 }
