5b89640b80b9c90ed4e17e3fb7cc2f0482a08016
DeanH1990 <dhasley1990@gmail.com>
2023-06-16 16:19:16 +0100

    add note list cards

--- ui/src/main/java/com/example/ui/ui/screens/home/HomeScreen.kt
+++ ui/src/main/java/com/example/ui/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.example.ui.ui.screens.home
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -10,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.notetakingapp.models.Note
 import com.example.ui.R
 import com.example.ui.ui.model.NoteUiState
 import com.example.ui.ui.theme.NoteTakingAppTheme
@@ -61,7 +64,7 @@ private fun HomeBody(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(8.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         if (noteList.isEmpty()) {
@@ -69,8 +72,53 @@ private fun HomeBody(
                 text = stringResource(R.string.empty_list),
                 style = MaterialTheme.typography.subtitle1
             )
+        } else {
+            NoteList(noteList = noteList)
         }
-    /* TODO add composable to show notes if database not empty     */
+    }
+}
+@Composable
+private fun NoteList(
+    noteList: List<NoteUiState>,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        content = {
+            items(noteList) { note ->
+                NoteCard(note = note, modifier = modifier)
+            }
+        }
+    )
+}
+
+@Composable
+private fun NoteCard(
+    note: NoteUiState,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .heightIn(min = 180.dp)
+    ) {
+        Column(
+            modifier = modifier.padding(8.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.h6
+            )
+            Spacer(modifier = modifier.height(8.dp))
+            Text(
+                text = note.truncatedContent,
+                style = MaterialTheme.typography.body1
+            )
+        }
     }
 }
 
