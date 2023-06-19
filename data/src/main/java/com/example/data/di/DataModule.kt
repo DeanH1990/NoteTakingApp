537fe659311ea0105e2d40fb8c1f021268026ac4
DeanH1990 <dhasley1990@gmail.com>
2023-06-19 13:48:54 +0100

    update note data repository name to note repository impl, update name in koin module

--- data/src/main/java/com/example/data/di/DataModule.kt
+++ data/src/main/java/com/example/data/di/DataModule.kt
@@ -2,7 +2,7 @@ package com.example.data.di
 
 import androidx.room.Room
 import com.example.data.database.NoteDatabase
-import com.example.data.repository.NoteDataRepository
+import com.example.data.repository.NoteRepositoryImpl
 import com.example.notetakingapp.repositories.NoteRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -18,5 +18,5 @@ val dataModule = module {
 
     single { get<NoteDatabase>().noteDao() }
 
-    single<NoteRepository> { NoteDataRepository(get()) }
+    single<NoteRepository> { NoteRepositoryImpl(get()) }
 }
