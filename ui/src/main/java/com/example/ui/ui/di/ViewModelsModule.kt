f8434003b4a00b94a55cbeaa8faeb8b9a0e22f62
DeanH1990 <dhasley1990@gmail.com>
2023-06-28 12:20:48 +0100

    add dependencies to koin modules

--- ui/src/main/java/com/example/ui/ui/di/ViewModelsModule.kt
+++ ui/src/main/java/com/example/ui/ui/di/ViewModelsModule.kt
@@ -1,6 +1,8 @@
 package com.example.ui.ui.di
 
+import androidx.lifecycle.SavedStateHandle
 import com.example.ui.ui.screens.home.HomeViewModel
+import com.example.ui.ui.screens.note_edit.NoteEditViewModel
 import com.example.ui.ui.screens.note_entry.NoteEntryViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,4 +10,7 @@ import org.koin.dsl.module
 val viewModelsModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { NoteEntryViewModel(get()) }
+    viewModel { (savedStateHandle: SavedStateHandle) ->
+        NoteEditViewModel(savedStateHandle, get(), get())
+    }
 }
