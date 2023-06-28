328d39696daf75f9fc85f8043cef3f6bd75f48d4
DeanH1990 <dhasley1990@gmail.com>
2023-06-28 12:17:05 +0100

    add note_edit destination route/args

---
+++ ui/src/main/java/com/example/ui/ui/screens/note_edit/NoteEditDestination.kt
@@ -0,0 +1,11 @@
+package com.example.ui.ui.screens.note_edit
+
+import com.example.ui.R
+import com.example.ui.ui.navigation.NavigationDestination
+
+object NoteEditDestination: NavigationDestination {
+    override val route = R.string.note_edit_route
+    override val titleRes = R.string.note_edit_title
+    const val noteIdArg = "noteId"
+    val routeWithArgs = "$route/${noteIdArg}"
+}
